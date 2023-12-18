a308392d62ade80397efb94303c694d4ceaecb4e
Zylphyhr <144801565+Zylphyhr@users.noreply.github.com>
2023-12-18 09:01:33 +0100

    Little cleanups, added base to package.json.

--- src/App.vue
+++ src/App.vue
@@ -25,16 +25,16 @@ onMounted(() => {
         <div class="col-lg-8 offset-lg-2 col-12">
           <ul class="nav nav-tabs "
               id="characterSheetTabs" role="tablist">
-            <li class="nav-item rounded-top mx-1 border border-white">
+            <li class="nav-item rounded-top mx-1 border border-white text-center">
               <router-link class="nav-link text-white" to="/stats">Stats & Skills</router-link>
             </li>
-            <li class="nav-item rounded-top mx-1 border border-white">
+            <li class="nav-item rounded-top mx-1 border border-white text-center">
               <router-link class="nav-link text-white" to="/combat">Combat</router-link>
             </li>
-            <li v-if="character && character.spells.length > 0" class="nav-item rounded-top mx-1 border border-white">
+            <li v-if="character && character.spells.length > 0" class="nav-item rounded-top mx-1 border border-white text-center">
               <router-link class="nav-link text-white" to="/spells">Spells</router-link>
             </li>
-            <li class="nav-item rounded-top mx-1 border border-white">
+            <li class="nav-item rounded-top mx-1 border border-white text-center">
               <router-link class="nav-link text-white" to="/actions">Actions</router-link>
             </li>
           </ul>
@@ -54,5 +54,10 @@ onMounted(() => {
 <style scoped>
 li {
   background-color: darkslategray;
+}
+.nav-item { width: 140px; }
+.nav-item:hover {
+  font-size: 1.05em;
+  transition: font-size 100ms ease-in-out;
 }
 </style>
